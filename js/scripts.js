eb760e4eb866464d2fde4dfb9e615494c9436651
Evgeny <djek.uait1998@gmail.com>
2023-04-11 14:49:06 +0300

    z-index commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,59 @@
-$(document).ready(function(){$(".header__burger").click(function(){$(".header__button").toggleClass("header__button--hide"),$(".header__links").toggleClass("header__links--open"),$(".header__line:nth-child(1)").toggleClass("header__line--first"),$(".header__line:nth-child(2)").toggleClass("header__line--last")})}),$(document).ready(function(){$(".header__button").click(function(){$(".header__button").toggleClass("header__button--active")})}),document.addEventListener("DOMContentLoaded",()=>{let n=document.querySelectorAll(".main__btn");n.forEach(function(e){e.addEventListener("click",()=>{n.forEach(e=>e.classList.remove("main__btn--active")),e.classList.add("main__btn--active")})})}),$(document).ready(function(){$(".popup__close").click(function(){$(".popup").toggleClass("popup--hide")}),$(".main__btn").click(function(){$(".popup").removeClass("popup--hide")}),$(".popup__background").click(function(){$(".popup").addClass("popup--hide")})});
+document.addEventListener('DOMContentLoaded', () => {
+
+    let myBtns=document.querySelectorAll('.main__btn');
+    myBtns.forEach(function(btn) {
+
+        btn.addEventListener('click', () => {
+          myBtns.forEach(b => b.classList.remove('main__btn--active'));
+          btn.classList.add('main__btn--active');
+        });
+ 
+    });
+
+});
+
+
+$(document).ready(function (){
+    $(".header__burger").click(function(){
+        $(".header__button").toggleClass("header__button--hide")
+        $(".header__links").toggleClass("header__links--open")
+        $(".header__line:nth-child(1)").toggleClass("header__line--first")
+        $(".header__line:nth-child(2)").toggleClass("header__line--last")
+    })
+
+})
+
+$(document).ready(function (){
+    $(".header__button").click(function(){
+        $(".header__button").toggleClass("header__button--active")
+    }) 
+
+})
+
+/*document.addEventListener('DOMContentLoaded', () => {
+
+    let myBtns=document.querySelectorAll('.header__button');
+    myBtns.forEach(function(btn) {
+
+        btn.addEventListener('click', () => {
+          myBtns.forEach(b => b.classList.remove('header__button--active'));
+          btn.classList.add('header__button--active');
+        });
+ 
+    });
+
+});*/
+
+$(document).ready(function (){
+    $(".popup__close").click(function(){
+        $(".popup").toggleClass("popup--hide")
+    })
+        
+    $(".main__btn").click(function(){
+        $(".popup").removeClass("popup--hide")
+    })
+
+    $(".popup__background").click(function(){
+        $(".popup").addClass("popup--hide")
+    })
+})
